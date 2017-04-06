a02b223fd34858e23f1cac445081ca67253f4414
Matt Martz <matt.martz@gmail.com>
2017-04-06 14:22:35 -0600

    step 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular Material Workshop';
+  users = [
+    {
+      name: 'Lia Lugo',
+      avatar: 'svg-11',
+      details: 'I love cheese, ...',
+      isAdmin: true,
+      isCool: false
+    },
+    {
+      name: 'George Duke',
+      avatar: 'svg-12',
+      details: 'Zombie ipsum ...',
+      isAdmin: false,
+      isCool: true
+    }
+  ];
 }
